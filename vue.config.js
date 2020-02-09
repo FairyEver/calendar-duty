bddc2ea66a389d0f9fc1a6e95e20d80332ff970c
FairyEver <1711467488@qq.com>
2020-02-09 10:43:40 +0800

    时间测试

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ process.env.VUE_APP_VERSION = require('./package.json').version
 // 构建时间 | 服务器时间转为中国时间
 // Unix 时间戳 (毫秒)
 let time = require('dayjs')()
-time = time.subtract(time.utcOffset(), 'minute').add(480, 'minute')
+// time = time.subtract(time.utcOffset(), 'minute').add(480, 'minute')
 process.env.VUE_APP_BUILD_TIME = `${time.valueOf()}|number`
 
 // 基础路径 注意发布之前要先修改这里
