7ef364104046558f770950106f7e5a17a43b74c8
FairyEver <1711467488@qq.com>
2020-02-09 10:29:04 +0800

    移除报错

--- src/filters/index.js
+++ src/filters/index.js
@@ -4,8 +4,6 @@ const files = require.context('./modules', false, /\.js$/)
 files.keys().forEach(key => {
   const filterModule = files(key).default
   for (const filterName in filterModule) {
-    if (filterModule.hasOwnProperty(filterName)) {
-      Vue.filter(filterName, filterModule[filterName])
-    }
+    Vue.filter(filterName, filterModule[filterName])
   }
 })
