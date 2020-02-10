b2bb9a5d767f630d4cda792b68cdc98641e20145
FairyEver <1711467488@qq.com>
2020-02-10 22:24:16 +0800

    优化任务列表显示

--- src/views/setting.vue
+++ src/views/setting.vue
@@ -5,8 +5,20 @@
 <template>
   <div class="setting">
     <van-cell-group title="任务">
-      <van-cell title="任务 1" is-link :to="{ name: 'setting-plan', params: { index: 0 } }"/>
-      <van-cell title="任务 2" is-link :to="{ name: 'setting-plan', params: { index: 1 } }"/>
+      <van-cell
+        :title="`任务 1 : ${$store.state.SETTING.PLAN[0].NAME}`"
+        is-link
+        :to="{
+          name: 'setting-plan',
+          params: { index: 0 }
+        }"/>
+      <van-cell
+        :title="`任务 2 : ${$store.state.SETTING.PLAN[1].NAME}`"
+        is-link
+        :to="{
+          name: 'setting-plan',
+          params: { index: 1 }
+        }"/>
     </van-cell-group>
     <van-cell-group title="显示">
       <van-cell title="日历" is-link :to="{ name: 'setting-display-calendar' }"/>
